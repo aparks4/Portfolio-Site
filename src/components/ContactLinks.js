0085b9fa3942727af09ab9edf9677f76e6b39e34
aparks4 <arynmparks@gmail.com>
2022-11-20 15:54:07 -0800

    mobile styling for about page and contact page

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -4,25 +4,25 @@ import pdf from '../Resume.pdf'
 function ContactLinks() {
     return (
         <div className='contact-links-container'>
-            <div>
+            <div className='contact-link'>
                 <a href="mailto:arynmparks@gmail.com" target='_blank' rel='noreferrer'>
                     <AiOutlineMail />
                 </a>
-                <p>arynmparks@gmail.com</p>
+                <p>arynmparks@gmail .com</p>
             </div>
-            <div>
+            <div className='contact-link'>
                 <a href="https://www.linkedin.com/in/aryn-parks/" target='_blank' rel='noreferrer'>
                     <AiFillLinkedin />
                 </a>
                 <p>aryn-parks</p>
             </div>
-            <div>
+            <div className='contact-link'>
                 <a href="http://www.github.com/aparks4" target='_blank' rel='noreferrer'>
                     <AiFillGithub />
                 </a>
                 <p>aparks4</p>
             </div>
-            <div>
+            <div className='contact-link'>
                 <a href={pdf} target='_blank' rel='noreferrer'>
                     <AiFillFileText />
                 </a>

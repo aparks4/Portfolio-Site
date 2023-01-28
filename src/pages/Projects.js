51e70b893c814bdd97d50272c9031afcb208ab22
aparks4 <arynmparks@gmail.com>
2023-01-27 21:06:26 -0800

    new url for incantations live site

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,7 +11,7 @@ function Projects(props) {
             <div className='projects-container'>
                 <ProjectCard id={'war-card'} name={'War'} liveSite={'https://aparks4.github.io/War-Browser-Game/'} code={'https://github.com/aparks4/War-Browser-Game'} description={'HTML | CSS | JavaScript'}/>
                 <ProjectCard id={'dbtrip-card'} name={'DB Trippin'} liveSite={'https://arynsgaproject.herokuapp.com/users/login'} code={'https://github.com/aparks4/DBTrippin'} description={'Express | Node.js | MongoDB | Mongoose'}/>
-                <ProjectCard id={'incantations-card'} name={'Incantations for Spotify'} liveSite={'https://632d4dba13263e422f1d773d--prismatic-kangaroo-4608c3.netlify.app/'} code={'https://github.com/aparks4/Incantations-for-Spotify'} description={'React | Express | Node.js | JWT'} />
+                <ProjectCard id={'incantations-card'} name={'Incantations for Spotify'} liveSite={'https://beamish-eclair-ddafee.netlify.app'} code={'https://github.com/aparks4/Incantations-for-Spotify'} description={'React | Express | Node.js | JWT'} />
                 <ProjectCard id={'elevation-card'} name={'Elevation Climbing'} liveSite={'https://github.com/aparks4/Elevation-Climbing-Frontend'} code={'https://github.com/aparks4/Elevation-Climbing-Backend'} description={'Django | Python | React | Node.js | PostgreSQL'} />
             </div>
         </div>

098f3d9a4c54d0b5720b75193c6e9d5bc1ae1fe6
aparks4 <arynmparks@gmail.com>
2023-03-28 12:42:20 -0700

    redesign projects page and components

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,45 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export const ProjectCard = (props) => {
+    const [hover, setHover] = useState(false);
+
+    const onMouseEnter = () => {
+        setHover(true);
+    }
+
+    const onMouseLeave = () => {
+        setHover(false);
+    }
+
     return (
         <>
-            <div className="project-card">
+            {/* <div className="project-card">
                 <h2>{props.name}</h2>
                 <p>{props.technologies}</p>
-                {/* <p>{props.description}</p> */}
                 <div className="project-links-wrapper">
                     <a href={props.site} target={'_blank'} rel={'noreferrer'}>Live Site</a>
                     <a href={props.code} target={'_blank'} rel={'noreferrer'}>Code</a>
                 </div>
+            </div> */}
+            <div className="project-wrapper" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                {hover ?
+                    <div className="project-hover-wrapper">
+                        <p>{props.description}</p>
+                        <div className="project-links-wrapper">
+                            <a href={props.site} target={'_blank'} rel={'noreferrer'}>Live Site</a>
+                            <a href={props.code} target={'_blank'} rel={'noreferrer'}>Code</a>
+                        </div>
+                    </div>
+                :
+                <>
+                    <div className="project-image" style={{'background-image': `url(${props.image})`}}></div>
+                    <div className="project-text-wrapper">
+                        <h2>{props.name}</h2>
+                        <p>{props.stack}</p>
+                    </div>
+                </>
+                }
             </div>
         
         </>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,10 +7,38 @@ export const ProjectsPage = (props) => {
         <>
             <div className="projects-page">
                 <div className="projects-container">
-                    <ProjectCard name={'Incantations for Spotify'} technologies={'React | Express | Node.js | JWT'} site={'https://beamish-eclair-ddafee.netlify.app/'} code={'https://github.com/basementlivin/Incantations-for-Spotify'} />
-                    <ProjectCard name={'DB Trippin'} technologies={'Express | Node.js | MongoDB | Mongoose'} site={'https://arynsgaproject.herokuapp.com/users/login'} code={'https://github.com/aparks4/DBTrippin'} />
-                    <ProjectCard name={'Talk-Thru'} technologies={'React | Express | TypeScript | MySQL'} site={'https://talkthru.netlify.app/'} code={'https://github.com/PeterSYoo/talkthru-frontend'} />
-                    <ProjectCard name={'Every Body Retreats'} technologies={'React | Node.js | Django | PostgreSQL'} site={'https://everybodyretreats.com'} code={'https://github.com/aparks4/every-body-site'} />
+                    <ProjectCard 
+                        name={'Every Body Retreats'} 
+                        stack={'React | Node.js | Django | PostgreSQL'}
+                        image={'https://i.imgur.com/bXK816W.png'}
+                        description={`A website for the nonprofit organization to house links to event registration forms (they use Google forms), create blog posts to share their story (in the works), and accept donations from interested parties (in the works).`} 
+                        site={'https://everybodyretreats.com'} 
+                        code={'https://github.com/aparks4/every-body-site'} 
+                    />
+                    <ProjectCard 
+                        name={'Talk-Thru'} 
+                        stack={'React | Express | TypeScript | MySQL'}
+                        image={'https://i.imgur.com/fhEYK7n.png'}
+                        site={'https://talkthru.netlify.app/'} 
+                        code={'https://github.com/PeterSYoo/talkthru-frontend'} 
+                        description={'A video chat app for paired learning and mentorship'}
+                    />
+                    <ProjectCard 
+                        name={'Incantations for Spotify'}  
+                        stack={'React | Express | Node.js | JWT'} 
+                        image={'https://i.imgur.com/fhEYK7n.png'}
+                        description={'A Halloween-themed playlist generator for Spotify users'}
+                        site={'https://beamish-eclair-ddafee.netlify.app/'} 
+                        code={'https://github.com/basementlivin/Incantations-for-Spotify'} 
+                    />
+                    <ProjectCard 
+                        name={'DB Trippin'} 
+                        stack={'Express | Node.js | MongoDB | Mongoose'} 
+                        image={'https://i.imgur.com/bXK816W.png'}
+                        description={'A web application for collaborative planning and travel blogging'}
+                        site={'https://arynsgaproject.herokuapp.com/users/login'} 
+                        code={'https://github.com/aparks4/DBTrippin'} 
+                    />
                 </div>
             </div>
         </>

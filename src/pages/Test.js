d7c88375587b01830d880a698d05126ae48e594d
aparks4 <arynmparks@gmail.com>
2023-04-02 11:30:40 -0600

    switched browserrouter to hashrouter

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -6,7 +6,9 @@ export const TestPage = () => {
             <div className="test-page">
                 <h1>aryn parks</h1>
                 <h2>full stack developer</h2>
-                <div className="border"></div>
+                <div className="lite-bg">
+                    <h2>About Me</h2>
+                </div>
 
             </div>
         
